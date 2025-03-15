f2266f0e8e1e02c9c2633ad7a8002138ae6b9dd0
the3t <thanhthuong12032005@gmail.com>
2025-03-15 10:42:55 +0700

    init

---
+++ app/src/main/java/com/example/mymusicplayer/model/Song.kt
@@ -0,0 +1,13 @@
+package com.example.mymusicplayer.model
+
+import android.net.Uri
+
+class Song(
+    val id: Long,
+    val title: String,
+    val artist: String?,
+    val album: String?,
+    val path: String,
+) {
+
+}
