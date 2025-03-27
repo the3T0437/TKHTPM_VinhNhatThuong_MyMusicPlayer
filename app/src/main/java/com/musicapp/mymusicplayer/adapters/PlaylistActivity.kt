0d87b740cd4de697d7d832ff4c35aa9947365de8
trongvinh2804 <trongvinh28042000@gmail.com>
2025-03-27 20:50:42 +0700

    relatedSongs

--- app/src/main/java/com/musicapp/mymusicplayer/adapters/PlaylistActivity.kt
+++ app/src/main/java/com/musicapp/mymusicplayer/adapters/PlaylistActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mymusicplayer.adapters
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.musicapp.mymusicplayer.databinding.PlaylistLayoutBinding
+
 
 class PlaylistActivity : AppCompatActivity() {
     private lateinit var binding: PlaylistLayoutBinding
