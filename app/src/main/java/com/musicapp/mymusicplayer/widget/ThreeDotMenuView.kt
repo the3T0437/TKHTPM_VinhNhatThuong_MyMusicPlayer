08a15b1a5e37323a6f58df7e245efbf19c518086
the3t <thanhthuong12032005@gmail.com>
2025-03-28 10:08:52 +0700

    change widget for ThreeDotMenuView

--- app/src/main/java/com/musicapp/mymusicplayer/widget/ThreeDotMenuView.kt
+++ app/src/main/java/com/musicapp/mymusicplayer/widget/ThreeDotMenuView.kt
@@ -5,19 +5,16 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageButton
 import android.widget.PopupMenu
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.musicapp.mymusicplayer.R
-import com.musicapp.mymusicplayer.databinding.ViewThreeDotMenuLayoutBinding
 
 interface ThreeDotMenuListener {
     fun onMenuItemClick(item: MenuItem): Boolean
 }
 
-class ThreeDotMenuView : ConstraintLayout {
-
-
-    private lateinit var binding: ViewThreeDotMenuLayoutBinding
+class ThreeDotMenuView : androidx.appcompat.widget.AppCompatImageButton{
     private var menuListener: ThreeDotMenuListener? = null
     private var menuResId: Int = R.menu.menu_song_options
     private var context: Context
@@ -39,21 +36,28 @@ class ThreeDotMenuView : ConstraintLayout {
         setupWidget()
     }
 
-    constructor(
-        context: Context,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes){
-        this.context = context
-        setupWidget()
-    }
-
     private fun setupWidget(){
+        /*
         binding = ViewThreeDotMenuLayoutBinding.inflate(LayoutInflater.from(context), this, true)
         binding.imgThreeDot.setOnClickListener{
             showPopupMenu()
         }
+
+         */
+
+        this.setOnClickListener{
+            showPopupMenu()
+        }
+    }
+
+    private fun showPopupMenu() {
+        val popupMenu = PopupMenu(this.context, this)
+        popupMenu.menuInflater.inflate(menuResId, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            menuListener?.onMenuItemClick(item) ?: false
+        }
+        popupMenu.show()
     }
 
     fun setMenuResource(menuResId: Int) {
@@ -62,15 +66,5 @@ class ThreeDotMenuView : ConstraintLayout {
 
     fun setThreeDotMenuListener(listener: ThreeDotMenuListener) {
         this.menuListener = listener
-    }
-
-    private fun showPopupMenu() {
-        val popupMenu = PopupMenu(binding.imgThreeDot.context, binding.imgThreeDot)
-        popupMenu.menuInflater.inflate(menuResId, popupMenu.menu)
-
-        popupMenu.setOnMenuItemClickListener { item ->
-            menuListener?.onMenuItemClick(item) ?: false
-        }
-        popupMenu.show()
     }
 }
