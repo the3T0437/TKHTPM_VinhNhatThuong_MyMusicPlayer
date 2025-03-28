548537dafc2df6cd459cf137f487b7af6d634207
the3t <thanhthuong12032005@gmail.com>
2025-03-28 09:47:11 +0700

    change three dot widget

--- app/src/main/java/com/musicapp/mymusicplayer/activities/PlaylistActivity.kt
+++ app/src/main/java/com/musicapp/mymusicplayer/activities/PlaylistActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mymusicplayer.adapters
+package com.musicapp.mymusicplayer.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.musicapp.mymusicplayer.databinding.PlaylistLayoutBinding
