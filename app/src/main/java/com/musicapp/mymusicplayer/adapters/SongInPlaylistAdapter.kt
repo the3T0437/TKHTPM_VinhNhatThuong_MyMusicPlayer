47ad59dad9e20bc02864fa754b3dc83880e927fd
the3t <thanhthuong12032005@gmail.com>
2025-04-15 21:55:09 +0700

    dragable song in playlist

---
+++ app/src/main/java/com/musicapp/mymusicplayer/adapters/SongInPlaylistAdapter.kt
@@ -0,0 +1,8 @@
+package com.musicapp.mymusicplayer.adapters
+
+import android.content.Context
+import com.musicapp.mymusicplayer.model.Song
+
+class SongInPlaylistAdapter(context: Context, arr: ArrayList<Song>) : DragableSongAdapter(context, arr){
+
+}
--- app/src/main/java/com/musicapp/mymusicplayer/database/SongDAO.kt
+++ app/src/main/java/com/musicapp/mymusicplayer/database/SongDAO.kt
@@ -11,19 +11,19 @@ import com.musicapp.mymusicplayer.model.Song
 @Dao
 interface SongDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun themSong(song:Song):Long
+    fun themSong(song:Song):Long
     @Query("SELECT * FROM ${Song.TABLE_NAME} order by ${Song.ID} ASC")
-    suspend fun getAllSongs():List<Song>
+    fun getAllSongs():List<Song>
     @Query("SELECT * FROM ${Song.TABLE_NAME} WHERE ${Song.ID} = :id")
-    suspend fun getSong(id: Long):Song?
+    fun getSong(id: Long):Song?
     @Update
-    suspend fun updateSong(song: Song):Int
+    fun updateSong(song: Song):Int
     @Query("DELETE FROM ${Song.TABLE_NAME} WHERE ${Song.ID} = :songId")
-    suspend fun deleteSong(songId: Long): Int
+    fun deleteSong(songId: Long): Int
 
     @Query("SELECT * FROM ${Song.TABLE_NAME} WHERE ${Song.TITLE} LIKE '%' || :title || '%'")
-    suspend fun searchSongs(title: String): List<Song>
+    fun searchSongs(title: String): List<Song>
 
     @Query("SELECT * FROM song WHERE album = (SELECT album FROM song WHERE id = :songId) AND id != :songId ORDER BY RANDOM() LIMIT 10")
-    suspend fun getRelatedSongs(songId: Long): List<Song>
+    fun getRelatedSongs(songId: Long): List<Song>
 }
